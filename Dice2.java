2384e4625d6a84124d5ce8e68195498bf357f2bb
maya708 <s1260186@u-aizu.ac.jp>
2019-07-11 12:04:11 +0900

    second revision

---
+++ Dice2.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Dice2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Random r = new Random();
+
+        System.out.println("What is your name?");
+        String str = s.nextLine();
+        System.out.println("Hello, " + str + "!");
+
+        System.out.println("Rolling the dice...");
+        int a = r.nextInt(6) + 1;
+        int b = r.nextInt(6) + 1;
+        System.out.println("Die 1: " + a);
+        System.out.println("Die 2: " + b);
+        System.out.println("Total value: " + (a + b));
+        s.close();
+
+    }
+}
